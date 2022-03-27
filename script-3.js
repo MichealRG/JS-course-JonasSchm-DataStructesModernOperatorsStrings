803e56896da6c231303133ea96e183b42674ccaa
Michał <KrzyzowskiM1998@gmail.com>
2022-03-27 13:29:12 +0200

    Intro to maps

--- script-3.js
+++ script-3.js
@@ -67,3 +67,34 @@ for (const order of ordersSet) console.log(order);
 // console.log(ordersSet); //Set[]
 
 console.log([...ordersSet]);
+
+//maps
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Fienze, Italy');
+rest.set(2, 'Lisbon, Portugal');
+console.log(rest);
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest);
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest);
+console.log(rest.size);
+// console.log(rest.clear());
+
+rest.set([1, 2], 'hello there');
+rest.get([1, 2]); //undefined
+
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
